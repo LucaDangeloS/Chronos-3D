e87ae18a7f114d27e9ba62b26e84cfd80f9eaac4
David Fandiño <d.fandinodovalo@gmail.com>
2023-05-03 00:12:12 +0200

    Add menu settings implementation

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    // public Dropdown resolutionDropdown;
+
+    // Resolution[] resolutions;
+
+    // void Start() {
+    //     resolutions = Screen.resolutions;
+
+    //     resolutionDropdown.ClearOptions();
+
+    //     List<string> options = new List<string>();
+
+    //     for (int i = 0; i < resolutions.Length; i++) {
+    //         string option = resolutions[i].width + " x " + resolutions[i].height;
+    //         options.Add(option);
+    //     }
+
+    //     resolutionDropdown.AddOptions(options);
+    // }
+
+    public void SetVolumen(float volume) {
+       audioMixer.SetFloat("volume", volume);
+    }
+
+
+    public void SetFullscreen(bool isFullScreen) {
+        Screen.fullScreen = isFullScreen;
+    }
+
+}
