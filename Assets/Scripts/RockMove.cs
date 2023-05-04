d5eca22e5d4a8cd75814fd4e16a2f586aaeeba09
David Fandiño <d.fandinodovalo@gmail.com>
2023-05-04 20:01:05 +0200

    Add rock movement

--- Assets/Scripts/RockMove.cs
+++ Assets/Scripts/RockMove.cs
@@ -9,12 +9,12 @@ public class RockMove : MonoBehaviour
     public bool move;
 
     public float speed = 0.02f;
+    public float moveSpeed = 0.05f;
     private float startPositionX;
     // Start is called before the first frame update
     void Start()
     {
-        float startPositionX = transform.position[0];
-                Debug.Log(startPositionX);
+        float startPositionX = transform.position[2];
 
     }
 
@@ -27,7 +27,19 @@ public class RockMove : MonoBehaviour
         }
 
         if (move) {
-            
+            // if (startPositionX > transform.position[2]+2 && startPositionX < transform.position[2]-2) {
+            //     Debug.Log("atpc");
+            // }
+
+            if (transform.position[2] < startPositionX + 2 && transform.position[2] > startPositionX - 2) {
+                moveSpeed = -moveSpeed;
+            } else {
+                Debug.Log("in");
+            }
+
+
+            transform.Translate(moveSpeed, 0, 0);
+            // Debug.Log("move");
         }
     }
 }
