ea429730c6d4939da9d35e50296d316b892f6214
LucaDangeloS <lucad7@gmail.com>
2023-05-09 13:21:09 +0200

    Added outline and fixed interpolation for slow motion movement

--- Assets/Scripts/Level/TimeControllableObject.cs
+++ Assets/Scripts/Level/TimeControllableObject.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
+using DG.Tweening;
 
 public class TimeControllableObject : MonoBehaviour
 {
     public float timeScale = 1f;
+    public float duration = 10f;
+    private float timeStep = 0.05f;
+
     private Rigidbody rb;
+    private Outline outline;
     private bool hasGravity = false;
     private Vector3 gravity = Physics.gravity;
     private Vector3 prevVelocity = Vector3.zero;
     private Vector3 velocityDelta = Vector3.zero;
+    private bool isReturningToSpeed = false;
     // Still buggy with object forces
     // Still need to stop the object rotation
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        outline = GetComponent<Outline>();
         hasGravity = rb.useGravity;
     }
     private void Update()
@@ -40,30 +47,53 @@ public class TimeControllableObject : MonoBehaviour
             {
                 rb.AddForce(gravity * timeScale, ForceMode.Acceleration);
             }
+
+            // Gradually return timeScale to 1f
+            if (Mathf.Abs(timeScale - 1f) < timeStep)
+                SetTimeScale(1f, false);
+            else if (timeScale < 1f)
+                SetTimeScale(timeScale + (timeStep / duration), false);
+            else if (timeScale > 1f)
+                SetTimeScale(timeScale - (timeStep / duration), false);
         }
         prevVelocity = rb.velocity;
     }
 
-    public void SetTimeScale(float newTime)
+    public void SetTimeScale(float newTime, bool firstSet = true)
     {
         if (rb == null)
         {
             return;
         }
 
-        if (timeScale == 1f)
+        if (newTime != 1f)
         {
             rb.useGravity = false;
+            if (firstSet)
+            {
+                rb.velocity = rb.velocity * newTime;
+                prevVelocity = prevVelocity * newTime;
+            } else
+            {
+                rb.velocity = rb.velocity * (1 + (timeScale - newTime));
+                prevVelocity = prevVelocity * (1 + (timeScale - newTime));
+            }
             timeScale = newTime;
-            rb.velocity = rb.velocity * timeScale;
-            prevVelocity = prevVelocity * timeScale;
+            if (outline != null)
+            {
+                float lerpAmount = Mathf.Clamp01((1f - timeScale) / 0.5f);
+                Color newColor = Color.Lerp(new Color(0, 0, 0, 0), Color.red, lerpAmount);
+                outline.ChangeColor(newColor);
+            }
         }
         else
         {
-            rb.useGravity = hasGravity ? true : false;
+            rb.useGravity = hasGravity;
             rb.velocity = rb.velocity / timeScale;
             prevVelocity = prevVelocity / timeScale;
             timeScale = 1f;
+            if (outline != null)
+                outline.ChangeColor(new Color(0, 0, 0, 0));
         }
     }
 }
