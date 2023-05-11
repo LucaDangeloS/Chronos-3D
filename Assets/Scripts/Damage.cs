65463588d83f34df7ad2932e7e55e5589860e713
David Fandiño <d.fandinodovalo@gmail.com>
2023-05-11 18:57:31 +0200

    Add healthBar to level1

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -1,5 +1,7 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
@@ -8,24 +10,56 @@ public class Damage : MonoBehaviour
     public Animator animator;
     private Animator player;
 
+    public HealthBar healthBar;
+
+    public GameObject gameOverScreen; 
+    public StarterAssetsInputs inputController;
+
     void Start()
     {
+        Time.timeScale = 1f;
         animator = GetComponent<Animator>();
         player = GameObject.FindWithTag("Player").GetComponent<Animator>();
+
+        if (healthBar != null) {
+            healthBar.SetMaxHealth(life);
+        }
     }
 
 
     public void TakeDamage(int dAmount) {
         life -= dAmount;
+        
+        if (healthBar != null) {
+            healthBar.SetHealth(life);
+        }
         if(life <= 0) {
             animator.SetTrigger("die");
-             if (animator != player) { 
+            if (animator != player) { 
                 Destroy(gameObject,1.5f);
+                return;
             }
+
+            StartCoroutine(DelayGameOverScreen(3f));
+            inputController.setGamePaused(true);
+
+
         } else {
             animator.SetTrigger("damage");
         }
     }
 
+
+    private void gameOver() {
+        gameOverScreen.SetActive(true);
+    }
+
+
+    IEnumerator DelayGameOverScreen(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Time.timeScale = 0f;
+        gameOver();
+    }
 
 }
