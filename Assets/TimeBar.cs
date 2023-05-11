96c994f6e7ee1cce8d4f49c9641f75ea7e92a7b5
David Fandiño <d.fandinodovalo@gmail.com>
2023-05-11 19:32:14 +0200

    Add time limited in level 2

---
+++ Assets/TimeBar.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using StarterAssets;
+
+
+
+public class TimeBar : MonoBehaviour
+{
+
+    public int levelSeconds;
+    public Slider slider;
+    private int remainingDuration;
+    public GameObject gameOverScreen;
+    public StarterAssetsInputs inputController;
+
+
+
+
+    public void SetMaxTime(int time) {
+        slider.maxValue = time;
+        slider.value = time;
+    }
+
+    public void SetTime(int Time) {
+        slider.value = Time;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainingDuration = levelSeconds;
+        SetMaxTime(levelSeconds);
+        StartCoroutine(UpdateTime());
+    }
+
+
+    private IEnumerator UpdateTime() {
+        while (remainingDuration >= 0 ) {
+            remainingDuration--;
+            slider.value = remainingDuration;
+            yield return new WaitForSeconds(1f);
+
+        }
+        onEnd();
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+
+    private void onEnd() {
+        Debug.Log("End count");
+        inputController.setGamePaused(true);
+        Time.timeScale = 0f;
+        gameOverScreen.SetActive(true);
+    }
+
+}
