2244a4630d262eca7d727c26389ee900c954aa83
alejandrabermo <alejandra.bermo@udc.es>
2023-05-12 13:53:11 +0200

    updated enemies code

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -5,72 +5,25 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    private float grade;
-    private int routine;
-    private float timer;
+   
     private Animator animator;
-    private Quaternion angle;
     private Tween movementTween;
     private Tween rotationTween;
-
-    public float minChaseD = 20;
-    public int  enemyLife = 100;
-    public float timeScale;
-
     public Transform player;
 
-    void Start() {
+  
+    public float timeScale;
+
+
+    protected virtual void  Start() {
         animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    public void enemyBehavior(){
-    
-        if (Vector3.Distance(player.position, animator.transform.position) > minChaseD) {
-            animator.SetBool("run", false);
-            animator.SetBool("attack", false);
-            timer += 1 * Time.deltaTime;
-            if (timer >= 4) {
-                routine = Random.Range(0,2);
-                timer = 0;
-            }
-
-            switch (routine) {
-                case 0: 
-                    animator.SetBool("walk", false);
-                    break;
-                case 1:
-                    grade = Random.Range(0, 360);
-                    angle = Quaternion.Euler(0, grade, 0);
-                    routine++;
-                    break;
-                case 2:
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
-                    transform.Translate(Vector3.forward * 1 * Time.deltaTime * timeScale);
-                    animator.SetBool("walk", true);
-                    break;
-            }
-        } else {
-            if (Vector3.Distance(player.position, animator.transform.position) > 2) {
-                var lookPos = player.position - transform.position;
-                lookPos.y = 0;
-                var rotation = Quaternion.LookRotation(lookPos);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
-                animator.SetBool("walk", false);
-                animator.SetBool("run", true);
-                transform.Translate(Vector3.forward * 3 * Time.deltaTime * timeScale);
-                animator.SetBool("attack", false);
-            } else {
-                animator.SetBool("walk", false);
-                animator.SetBool("run", false);
-                animator.SetBool("attack", true);
-            }
-        }
-    }
-
     public void AttackPlayer() {
-        player.GetComponent<Damage>().TakeDamage(10);
+        player.GetComponent<Damage>().TakeDamage(20);
     }
+
 
     void Update(){
         animator.speed = timeScale;
@@ -78,6 +31,10 @@ public class Enemies : MonoBehaviour
             rotationTween.timeScale = timeScale;
         if (movementTween != null)
             movementTween.timeScale = timeScale;
-        enemyBehavior();
+        enemyBehavior(player, animator);
+    }
+
+    protected virtual void enemyBehavior(Transform player, Animator animator) {
+        
     }
 }
